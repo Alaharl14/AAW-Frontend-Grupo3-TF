aa8b031778fc44f3ffe1342b0e8b83791c669a00
Alaharl14 <u202013176@upc.edu.pe>
2022-11-22 04:41:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AAW-Frontend-Grupo3
+# AAW-Frontend-Grupo3
+# AAW-Frontend-Grupo3-TF
---
+++ src/app/service/reporte.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { EMPTY, Subject } from 'rxjs';
+import { Reporte } from '../model/reporte';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReporteService {
+  private url: string = "http://localhost:8080/reporte"
+  private listaCambio = new Subject<Reporte[]>()
+  private confirmaEliminacion = new Subject<Boolean>()
+  constructor(private http: HttpClient) { }
+
+  listar() {
+    return this.http.get<Reporte[]>(this.url);
+  }
+
+  insertar(reporte: Reporte) {
+
+    return this.http.post(this.url, reporte);
+  }
+
+  modificar(reporte: Reporte) {
+
+    return this.http.put(this.url, reporte);
+  }
+
+  eliminar(id: number) {
+
+    return this.http.delete(`${this.url}/${id}`);
+  }
+
+  buscar(texto:string) {
+    return this.http.post<Reporte[]>(`${this.url}/buscar`, texto);
+  }
+
+  buscar1(texto:string) {
+    console.log("algo")
+  if (texto.length != 0) {
+   return this.http.post<Reporte[]>(`${this.url}/buscar`, texto.toLowerCase());
+  }
+    return EMPTY;
+  }
+
+  listarId(id: number) {
+    return this.http.get<Reporte>(`${this.url}/${id}`);
+  }
+
+  getLista() {
+    return this.listaCambio.asObservable();
+  }
+
+  setLista(listaNueva: Reporte[]) {
+    this.listaCambio.next(listaNueva);
+  }
+
+  getConfirmaEliminacion() {
+    return this.confirmaEliminacion.asObservable();
+  }
+
+  setConfirmaEliminacion(estado: Boolean) {
+    this.confirmaEliminacion.next(estado);
+  }
+
+
+}
